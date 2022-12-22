086502918302d9218d263f39584cb6802a0853fe
royb <roy.basmacier@primekey.com>
2022-12-22 12:31:37 -0500

    fixed sphincsplus privatekey recovery test

--- core/src/main/java/org/bouncycastle/pqc/crypto/util/PrivateKeyInfoFactory.java
+++ core/src/main/java/org/bouncycastle/pqc/crypto/util/PrivateKeyInfoFactory.java
@@ -152,7 +152,6 @@ public class PrivateKeyInfoFactory
             SPHINCSPLUSPrivateKey spPriv = new SPHINCSPLUSPrivateKey(0, params.getSeed(), params.getPrf(), spPub);
 
             return new PrivateKeyInfo(algorithmIdentifier, spPriv, attributes);
-//            return new PrivateKeyInfo(algorithmIdentifier, new DEROctetString(encoding), attributes, pubEncoding);
         }
         else if (privateKey instanceof PicnicPrivateKeyParameters)
         {
